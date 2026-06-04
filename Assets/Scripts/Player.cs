619fc7b1cb07a18d44189745069018741bf9167b
asato <>
2026-06-05 03:34:59 +0900

    Index参照にて苦戦中...。

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,20 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] Arrays arrays;
     private Transform myTrans;
     private Vector3 moveVect;
     private const float speed = 10.0f;
+    private int haveIndex;
     private bool isPauseMode;
 
     void Start()
     {
         myTrans = this.GetComponent<Transform>();
         moveVect = Vector3.zero;
+        haveIndex = -1;
         isPauseMode = false;
     }
 
@@ -18,27 +22,49 @@ public class Player : MonoBehaviour
     {
         moveVect = Vector3.zero;
 
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             isPauseMode = !isPauseMode;
-            Time.timeScale = isPauseMode? 0.0f:1.0f;
+            Time.timeScale = isPauseMode ? 0.0f : 1.0f;
         }
 
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
             moveVect += Vector3.forward;
 
-        if(Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
             moveVect += Vector3.back;
 
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
             moveVect += Vector3.left;
 
-        if(Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
             moveVect += Vector3.right;
 
-        if(moveVect != Vector3.zero)
+        if (moveVect != Vector3.zero)
         {
             myTrans.position += moveVect * (speed * Time.deltaTime);
         }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        switch (collider.gameObject.tag)
+        {
+            case "package":
+                if (haveIndex == -1)
+                {
+                    haveIndex = Array.IndexOf(arrays.packages, collider.gameObject);
+                    if (haveIndex == -1)
+                    {
+                        Debug.LogError("配列に無い荷物です。");
+                        return;
+                    }
+                    collider.gameObject.SetActive(false);
+                }
+                else
+                    Debug.Log("荷物は取得済みです。");
+                break;
+        }
+
     }
 }
